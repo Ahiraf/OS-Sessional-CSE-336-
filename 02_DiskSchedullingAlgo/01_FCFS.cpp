8363309ec8bc21f813e2de93453b4a3b56ea45e4
Fariha Rayhan Mim <168169066+Ahiraf@users.noreply.github.com>
2026-05-30 23:10:10 +0600

    Create 01_FCFS.cpp

---
+++ 02_DiskSchedullingAlgo/01_FCFS.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"Enter number of requests: ";
+    cin>>n;
+
+    vector<int> request(n);
+
+    cout<<"Enter request sequence: ";
+    for(int i=0;i<n;i++){
+        cin>>request[i];
+    }
+
+    int head;
+    cout<<"Enter initial head position: ";
+    cin>>head;
+
+    int seek_time=0;
+
+    cout<<"\nFCFS Order: ";
+
+    for(int i=0;i<n;i++){
+        cout<<request[i]<<" ";
+        seek_time+=abs(head-request[i]);
+        head=request[i];
+    }
+
+    cout<<"\nTotal Seek Time: "<<seek_time;
+
+    return 0;
+}
