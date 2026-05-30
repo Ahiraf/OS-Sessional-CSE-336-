205ddaa0701b9ff6f6c63d38e660ab712162ecc4
Fariha Rayhan Mim <168169066+Ahiraf@users.noreply.github.com>
2026-05-30 23:16:58 +0600

    Add files via upload

---
+++ 02_DiskSchedulingAlgo/05_LOOK.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"Enter number of requests: ";
+    cin>>n;
+
+    vector<int> request(n);
+
+    cout<<"Enter request sequence: ";
+    for(int i=0;i<n;i++){
+        cin>>request[i];
+    }
+
+    int head;
+
+    cout<<"Enter initial head position: ";
+    cin>>head;
+
+    vector<int> left,right;
+
+    for(int i=0;i<n;i++){
+        if(request[i]<head)
+            left.push_back(request[i]);
+        else
+            right.push_back(request[i]);
+    }
+
+    sort(left.begin(),left.end());
+    sort(right.begin(),right.end());
+
+    int seek_time=0;
+
+    cout<<"\nLOOK Order: ";
+
+    for(int i=0;i<right.size();i++){
+        cout<<right[i]<<" ";
+        seek_time+=abs(head-right[i]);
+        head=right[i];
+    }
+
+    for(int i=left.size()-1;i>=0;i--){
+        cout<<left[i]<<" ";
+        seek_time+=abs(head-left[i]);
+        head=left[i];
+    }
+
+    cout<<"\nTotal Seek Time: "<<seek_time;
+
+    return 0;
+}
